b586091500e366a51bde44378d78d26ae1139568
Marco <marco.mojica@hotmail.de>
2018-05-21 14:54:44 +0200

    Extract canvas into room

---
+++ components/Room.js
@@ -0,0 +1,42 @@
+import react from 'react'
+import styled from 'styled-components'
+
+import GuiOverlay from '../components/guiOverlay'
+
+const Background = styled.div`
+display: flex;
+background-color: #00ff00;
+justify-content: center;
+align-items: center;
+
+width: 100vw;
+height: 100vh;
+`
+
+const Canvas = styled.canvas`
+background-color: yellow;
+width: 100vw;
+height: 56.25vw;
+
+max-height: 100vh;
+max-width: 177.78vh;
+`
+
+export default class Room extends react.Component{
+
+    async componentDidMount () {
+        import("../src/game").then(main => {
+            window.onload = main.default
+        })
+    }
+
+    render() {
+        return (
+            <Background>
+                <Canvas id="myCanvas" width="1920" height="1080"/>
+                <GuiOverlay />
+            </Background>
+        )
+            
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,12 @@
 import react from 'react'
 import styled from 'styled-components'
 
-import GuiOverlay from '../components/guiOverlay'
-
-const Background = styled.div`
-    display: flex;
-    background-color: #00ff00;
-    justify-content: center;
-    align-items: center;
-
-    width: 100vw;
-    height: 100vh;
-`
-
-const Canvas = styled.canvas`
-    background-color: yellow;
-    width: 100vw;
-    height: 56.25vw;
-
-    max-height: 100vh;
-    max-width: 177.78vh;
-`
+import Room from '../components/Room'
 
 export default class Index extends react.Component{
-
-    async componentDidMount () {
-        import("../src/game").then(main => {
-            window.onload = main.default
-        })
-    }
-
     render() {
         return (
-                <Background>
-                    <Canvas id="myCanvas" width="1920" height="1080"/>
-                    <GuiOverlay />
-                </Background>
-            )
+            <Room />
+        )
     }
 }
