fa38d1bd44e75112eaeab7499918e097453b3d36
Marco <marco.mojica@hotmail.de>
2018-05-21 15:56:27 +0200

    Add room routing

--- components/CreateJoinRoom.js
+++ components/CreateJoinRoom.js
@@ -1,25 +1,55 @@
+import react from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    height: 20vh;
 
-    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
 `
 
 const ButtonContainer = styled.div`
+    width: 100%;
+
     display: flex;
     justify-content: space-around;
 `
 
-export default () => 
-    <Container>
-        <div>
-            <input type='text' />
-            <ButtonContainer>
-                <button>Join</button>
-                <button>Create</button>
-            </ButtonContainer>
-        </div>
-    </Container>
+export default class CreateJoinRoom extends react.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            roomName: ''
+        }
+    }
+
+    handleInput(event){
+        this.setState({
+            roomName: event.target.value
+        })
+    }
+    
+    render(){
+        return (
+            <Container>
+                <label>
+                    {'Room '}
+                    <input
+                    type='text'
+                    onChange={event => this.handleInput(event)}/>
+                </label>
+                <ButtonContainer>
+                    <Link 
+                        href={`/room?name=${this.state.roomName}`}
+                        as={`/room/${this.state.roomName}`}>
+                        <button>Join//Create</button>
+                    </Link>
+                </ButtonContainer>
+            </Container>
+        )
+    }
+}
