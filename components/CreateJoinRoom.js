b2670c15ae9b705e33c09f83bd8d8470dba759da
Marco <marco.mojica@hotmail.de>
2018-10-20 20:28:58 +0200

    Add Logo to index page

--- components/CreateJoinRoom.js
+++ components/CreateJoinRoom.js
@@ -1,5 +1,3 @@
-import react from 'react'
-import autoBind from 'react-autobind'
 import styled from 'styled-components'
 import Link from 'next/link'
 
@@ -12,46 +10,17 @@ const Container = styled.form`
     align-items: center;
 `
 
-const ButtonContainer = styled.div`
-    width: 100%;
-
-    display: flex;
-    justify-content: space-around;
-`
-
-export default class CreateJoinRoom extends react.Component {
-    constructor(props) {
-        super(props)
-        autoBind(this)
-
-        this.state = {
-            roomId: ''
-        }
-    }
-
-    render() {
-        return (
-            <Container>
-                Enter a Name to create or join a Room.
-                <input
-                    type='text'
-                    value={this.state.roomId}
-                    onChange={this.handleInput} />
-                <ButtonContainer>
-                    <Link
-                        prefetch
-                        href={`/room?roomId=${this.state.roomId}`}
-                        as={`/room/${this.state.roomId}`}>
-                        <button>Join//Create</button>
-                    </Link>
-                </ButtonContainer>
-            </Container>
-        )
-    }
-
-    handleInput(event) {
-        this.setState({
-            roomId: event.target.value.toLowerCase()
-        })
-    }
-}
+export default props =>
+    <Container>
+        Enter a Name to create or join a Room.
+        <input
+            type='text'
+            value={props.roomId}
+            onChange={props.onChange} />
+        <Link
+            prefetch
+            href={`/room?roomId=${props.roomId}`}
+            as={`/room/${props.roomId}`}>
+            <button>Create//Join</button>
+        </Link>
+    </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,44 @@
 import react from 'react'
+import autoBind from 'react-autobind'
 import styled from 'styled-components'
+import Link from 'next/link'
 
-import CreateJoinRoom from '../components/CreateJoinRoom'
+import CreateJoinRoom from "../components/CreateJoinRoom"
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
     height: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
 `
 
-export default class Index extends react.Component{
+export default class Index extends react.Component {
+    constructor(props) {
+        super(props)
+        autoBind(this)
+
+        this.state = {
+            roomId: ''
+        }
+    }
+
     render() {
         return (
             <Container>
-                <CreateJoinRoom />
+                <img src="/static/C.svg" />
+                <CreateJoinRoom
+                    roomId={this.state.roomId}
+                    onChange={this.handleInput} />
+                <Link href="http://marco.mojica.de">Marco Mojica</Link>
             </Container>
         )
+    }
+
+    handleInput(event) {
+        this.setState({
+            roomId: event.target.value.toLowerCase()
+        })
     }
 }
