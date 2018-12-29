03923752e36aa24c3288fc263ca3c9efa8d1308e
Marco <marco.mojica@hotmail.de>
2018-12-29 15:08:48 +0100

    Use uniform to set color decay

--- src/AlphaFilter.js
+++ src/AlphaFilter.js
@@ -2,12 +2,15 @@ import { Filter } from "pixi.js"
 
 const FragShader = `
   varying vec2 vTextureCoord;
+  
   uniform sampler2D uSampler;
+  uniform float colorDecay;
+
   void main(void)
   {
     vec4 pixel = texture2D(uSampler, vTextureCoord);
     if(all(equal(pixel.rgb, vec3(0.0, 0.0, 0.0)))){
-      pixel.rgb += vec3(0.04, 0.04, 0.04);
+      pixel.rgb += vec3(colorDecay, colorDecay, colorDecay);
     }
 
     gl_FragColor = pixel;
@@ -15,8 +18,8 @@ const FragShader = `
 `
 
 class AlphaFilter extends Filter {
-  constructor(){
-    super(null, FragShader)
+  constructor(colorDecay){
+    super(null, FragShader, {colorDecay: {type: "float", value: colorDecay}})
   }
 }
 
