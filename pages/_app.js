d4cae648529bd619ec6ce3e40006e51dbf1bbf38
Marco <marco@mojica.de>
2019-09-02 23:05:06 +0200

    Fix Automatic Prerendering warning

--- pages/_app.js
+++ pages/_app.js
@@ -21,16 +21,6 @@ const GlobalStyle = createGlobalStyle`
 `
 
 class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps, reduxStore } = this.props
     return (
